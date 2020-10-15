d5e5c14c4c6bb27a89e023122cba383601c1e373
HernanZ <hernanjosuezavala@hotmail.com>
2020-10-15 10:37:18 -0400

    pretty  much done with this class. it passes all of the test from the document. the getHash method was added since i had to modify the neighbors method to return only the values array of the hash. This is where in connect it was conflicting since it was returning a hash so the getHash method fixes it by returning the hash for neighbors instead of the values array

--- starter-code/Cave.rb
+++ starter-code/Cave.rb
@@ -34,18 +34,62 @@ class Cave
       rooms[i-1]
     end
     def random_room
+        return @rooms.sample
+    end
+    def move(symbol, room, new_room)
+        room.remove(symbol)
+        new_room.add(symbol)
+    end
+    def add_hazard(symbol, number)
+        count =0
+        while count != number 
+            room = random_room
+            if !(room.has?(symbol))
+                room.add(symbol)
+                count += 1
+            end 
+        end
+    end
+
+    def room_with(symbol)
+        result = @rooms.select{
+            |room|
+            room.has?(symbol)
+        }
+        if !(result.empty?) 
+            return result[0]
+        end
+    end
+    def entrance 
+        @rooms.each {
+            |room|
+            if room.safe?
+                return room
+            end
+        }
     end
     attr_reader :rooms
+    private :initialize
 end
 
 cave = Cave.dodecahedron
-rooms = (1..20).map { |i| cave.room(i)}
-rooms.each do |room|
- print room.neighbors.count == 3, "\n"
- room.neighbors.each { |i|
-print i.neighbors.include?(room) == true , "\n"
- }
-end
+cave.add_hazard(:guard, 1)
+cave.add_hazard(:pit, 3)
+cave.add_hazard(:bats, 3)
+entrance = cave.entrance
+puts entrance.safe? == true
+entrance.neighbors.each{
+    |x|
+    puts x.hazards.size , " neightbors hazards size"
+}
+
+# rooms = (1..20).map { |i| cave.room(i)}
+# rooms.each do |room|
+#  print room.neighbors.count == 3, "\n"
+#  room.neighbors.each { |i|
+# print i.neighbors.include?(room) == true , "\n"
+#  }
+# end
 
 #test the dedoblabla pattern
 # rooms= cave.rooms
